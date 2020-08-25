d0e24155599ae69813103af71a33c4b61fc1b760
walgreen <walgreen.melo@gmail.com>
2020-08-25 15:05:49 -0300

    enter

--- mqtt_connection/mqtt.js
+++ mqtt_connection/mqtt.js
@@ -1,26 +1,26 @@
 // import mqtrtuyguytyut
 
-var mqtt = require('mqtt');
+// var mqtt = require('mqtt');
 
-var client = mqtt.connect('mqtt://io.adafruit.com', {
-    username: 'ADAFRUIT_IO_USERNAME',
-    password: 'ADAFRUIT_UI_KEY'
-});
+// var client = mqtt.connect('mqtt://io.adafruit.com', {
+//     username: 'ADAFRUIT_IO_USERNAME',
+//     password: 'ADAFRUIT_UI_KEY'
+// });
 
-var ssd1306topic = `${client.options.username}/f/ssd1306`;
+// var ssd1306topic = `${client.options.username}/f/ssd1306`;
 
-client.on('connect', function() {
-    console.log('connected');
+// client.on('connect', function() {
+//     console.log('connected');
 
-    client.subscribe(ssd1306topic, function(err) {
-        if(! err) {
-            console.log('subscribed');
+//     client.subscribe(ssd1306topic, function(err) {
+//         if(! err) {
+//             console.log('subscribed');
 
-            client.publish(ssd1306topic, 'Hello from NodeJS');
-        }
-    });
-});
+//             client.publish(ssd1306topic, 'Hello from NodeJS');
+//         }
+//     });
+// });
 
-client.on('message', function(topic, message) {
-    console.log(message.toString());
-});
+// client.on('message', function(topic, message) {
+//     console.log(message.toString());
+// });
