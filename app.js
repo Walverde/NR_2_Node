47be8b52a48aaf0d18b0a33223000041f6711e02
walgreen <walgreen.melo@gmail.com>
2020-08-25 20:08:25 -0300

    enter

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const app = express();
+
+const path = __dirname + '/views/'; // instanciando endereço dos aquivos estatics
+const port = 80;
+
+app.use(express.static(path)); // enviando os arquivos Estaticos
+app.use('/', router);
+
+app.use(express.urlencoded())
+
+router.use(function (req,res,next) {
+    next();
+});
+  
+router.get('/', function(req,res){
+    res.sendFile(path + 'index.html');
+});
+
+app.post('/enviar', function(req,res){
+    var msg = req.body.mensaje
+    var broker = req.body.broker
+    if(broker.search('mqtt://') == -1) {
+        broker = 'mqtt://' + broker;
+    }
+    var topic = req.body.topic
+    if(topic.length > 0) {
+
+        var mqtt = require('mqtt')
+
+        var client  = mqtt.connect(broker);
+        client.on('connect', function () {
+            
+            client.publish(topic, msg)
+            console.log("Mensaje enviado");
+            client.end();
+        })
+                
+        res.sendFile(path + 'index.html');
+    }
+});
+  
+
+
+app.listen(port, function () {
+    console.log('Ejecutando en puerto 80: http://localhost')
+})
