f992c5e7383512d05a50f186893ac6e87927b0a0
walgreen <walgreen.melo@gmail.com>
2020-08-25 15:11:50 -0300

    enter

--- mqtt_connection/mqtt.js
+++ mqtt_connection/mqtt.js
@@ -1,26 +1,17 @@
-// import mqtrtuyguytyut
+const mqtt = require('mqtt')
+const client  = mqtt.connect('localhost') // host, pode ser o ip tbm: ex: '192.168.0.31'
 
-// var mqtt = require('mqtt');
 
-// var client = mqtt.connect('mqtt://io.adafruit.com', {
-//     username: 'ADAFRUIT_IO_USERNAME',
-//     password: 'ADAFRUIT_UI_KEY'
-// });
+client.on('connect', function () {
+    client.subscribe('presence', function (err) {
+      if (!err) {
+        client.publish('presence', 'Hello mqtt')
+      }
+    })
+  })
 
-// var ssd1306topic = `${client.options.username}/f/ssd1306`;
-
-// client.on('connect', function() {
-//     console.log('connected');
-
-//     client.subscribe(ssd1306topic, function(err) {
-//         if(! err) {
-//             console.log('subscribed');
-
-//             client.publish(ssd1306topic, 'Hello from NodeJS');
-//         }
-//     });
-// });
-
-// client.on('message', function(topic, message) {
-//     console.log(message.toString());
-// });
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    client.end()
+  })  
