1ec3c12fe271703fc2cf7b7fa9fb87304b7bf8d4
walgreen <walgreen.melo@gmail.com>
2020-08-25 19:33:47 -0300

    Update mqtt.js

--- mqtt_connection/mqtt.js
+++ mqtt_connection/mqtt.js
@@ -1,11 +1,12 @@
 const mqtt = require('mqtt')
-const client  = mqtt.connect('localhost') // host, pode ser o ip tbm: ex: '192.168.0.31'
+const localhost = '192.168.100.6'
+const client  = mqtt.connect(localhost) // host, pode ser o ip tbm: ex: '192.168.0.31'
 
 
 client.on('connect', function () {
-    client.subscribe('presence', function (err) {
+    client.subscribe('node', function (err) {
       if (!err) {
-        client.publish('presence', 'Hello mqtt')
+        client.publish('node', 'Hello mqtt')
       }
     })
   })
