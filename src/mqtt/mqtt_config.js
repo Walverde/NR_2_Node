8e03cc0b23b58fcaccd2db3face2a700e18db32f
walgreen <walgreen.melo@gmail.com>
2020-08-30 14:40:55 -0300

    enter

--- src/mqtt/mqtt_config.js
+++ src/mqtt/mqtt_config.js
@@ -1,7 +1,9 @@
 var mqtt = require('mqtt'); //https://www.npmjs.com/package/mqtt
-var Topic = '#'; //subscribe to all topics
-var Broker_URL = 'mqtt://192.168.1.123';
-var Database_URL = '192.168.1.123';
+// var Topic = '#'; //subscribe to all topics
+var Topic = 'node'; //subscribe to all topics
+// var Broker_URL = 'mqtt://192.168.1.123';
+var Broker_URL = 'mqtt://localhost';
+var Database_URL = 'localhost';
 
 var options = {
 	clientId: 'MyMQTT',
@@ -45,7 +47,8 @@ function after_publish() {
 
 //receive a message from MQTT broker
 function mqtt_messsageReceived(topic, message, packet) {
-	var message_str = message.toString(); //convert byte array to string
+    var message_str = message.toString(); //convert byte array to string
+    
 	message_str = message_str.replace(/\n$/, ''); //remove new line
 	//payload syntax: clientID,topic,message
 	if (countInstances(message_str) != 1) {
@@ -58,49 +61,4 @@ function mqtt_messsageReceived(topic, message, packet) {
 
 function mqtt_close() {
 	//console.log("Close MQTT");
-};
-
-////////////////////////////////////////////////////
-///////////////////// MYSQL ////////////////////////
-////////////////////////////////////////////////////
-var mysql = require('mysql'); //https://www.npmjs.com/package/mysql
-//Create Connection
-var connection = mysql.createConnection({
-	host: Database_URL,
-	user: "newuser",
-	password: "mypassword",
-	database: "mydb"
-});
-
-connection.connect(function(err) {
-	if (err) throw err;
-	//console.log("Database Connected!");
-});
-
-//insert a row into the tbl_messages table
-function insert_message(topic, message_str, packet) {
-	var message_arr = extract_string(message_str); //split a string into an array
-	var clientID= message_arr[0];
-	var message = message_arr[1];
-	var sql = "INSERT INTO ?? (??,??,??) VALUES (?,?,?)";
-	var params = ['tbl_messages', 'clientID', 'topic', 'message', clientID, topic, message];
-	sql = mysql.format(sql, params);	
-	
-	connection.query(sql, function (error, results) {
-		if (error) throw error;
-		console.log("Message added: " + message_str);
-	}); 
-};	
-
-//split a string into an array of substrings
-function extract_string(message_str) {
-	var message_arr = message_str.split(","); //convert to array	
-	return message_arr;
-};	
-
-//count number of delimiters in a string
-var delimiter = ",";
-function countInstances(message_str) {
-	var substrings = message_str.split(delimiter);
-	return substrings.length - 1;
 };
