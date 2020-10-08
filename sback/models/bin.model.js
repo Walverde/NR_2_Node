757436a17ccaed2f2da68f31aa5076ab7bf4f3c7
walgreen <walgreen.melo@gmail.com>
2020-10-08 15:57:36 -0300

    enter

---
+++ sback/models/bin.model.js
@@ -0,0 +1,43 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, Sequelize) => {
+	const Control = sequelize.define('EnertrackData', {
+		id_nome: { type: Sequelize.INTEGER, },
+		VARMS: {
+			type: Sequelize.INTEGER,
+		},
+		VBRMS: {
+			type: Sequelize.INTEGER,
+		},
+		VCRMS: {
+			type: Sequelize.INTEGER,
+		},
+		// Corrente. 
+		IARMS: {
+			type: Sequelize.INTEGER,
+		},
+		IBRMS: {
+			type: Sequelize.INTEGER,
+		},
+		ICRMS: {
+			type: Sequelize.INTEGER,
+		},
+		PTA: { type: Sequelize.INTEGER, },
+		PTB: { type: Sequelize.INTEGER, },
+		PTC: { type: Sequelize.INTEGER, },
+		//VA
+
+		FPA: {
+			type: Sequelize.INTEGER,
+		},
+		FPB: {
+			type: Sequelize.INTEGER,
+		},
+		FPC: {
+			type: Sequelize.INTEGER,
+		},
+
+	});
+
+	return Control;
+}
