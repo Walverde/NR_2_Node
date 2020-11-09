dfb9a1b3d92ef2e4845c689e8884d9b759cf0b9c
Walgreen <walgreen.melo@gmail.com>
2020-11-09 13:07:08 -0300

    enter

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const configs = require('./back/settings/data/controller/dbs.controller')
 const mconctrl = require('./back/controllers/+ctrl.controller')
 //Cor-----------------------------------------------------------------
 const cor = require('./back/settings/cores')
-
 // Mqtt --------------------------------------------------------------
 const mqtts = require('./back/mqtt/config/mqttconfig')
-
 const cav = require('./cav')
 //--------------------------------------------------------------------
-
 const clientbin = mqtts.connecting
 const clientcon = mqtts.connecting
 const env = mqtts.envs
@@ -22,24 +19,21 @@ const env = mqtts.envs
 // var client = mqtt.connect(, options);
 
 clientcon.on('connect', function () {
-    clientcon.subscribe(env.Topic, function () { // Tentativa de incersao em dois topicos. 
+    clientcon.subscribe(env.Topic, function () {
         // Quando a messagem chegar, esse função sera executada. 
         clientcon.on('message', function (topic, message, packet) {
             var message_str = JSON.parse(message)
-            // console.log('Imprimindo do APP ->' ,message_str)
             console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgGreen}ConCtrl${cor.Reset}`)
             //inserindo no bando de dados os dados recibidos no MQTT
             mconctrl.InsertsMqtt(message_str)
         });
     });
 })
-
 clientcon.on('connect', function () {
-    clientcon.subscribe(env.Topic, function () { // Tentativa de incersao em dois topicos. 
+    clientcon.subscribe(env.Topic, function () { 
         // Quando a messagem chegar, esse função sera executada. 
         clientcon.on('message', function (topic, message, packet) {
             var message_str = JSON.parse(message)
-            // console.log('Imprimindo do APP ->' ,message_str)
             console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgRed}BinCtrl${cor.Reset}`)
             //inserindo no bando de dados os dados recibidos no MQTT
             binctrl.InsertsMqtt(message_str)
