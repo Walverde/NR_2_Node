74517c6fb5635e841ff80bcd8bcbc1478623a5be
Walgreen <walgreen.melo@gmail.com>
2020-11-10 10:44:12 -0300

    tetando logica de minuto.

--- app.bk.js
+++ app.bk.js
@@ -1,20 +1,14 @@
 // Databases ---------------------------------------------------------
 const binctrl = require('./back/controllers/bintrack.controller')
-// const conctrl = require('./mback/controllers/ctrl.controller')
+// const configs = require('./back/settings/data/controller/dbs.controller')
 const mconctrl = require('./back/controllers/+ctrl.controller')
-
+const binuto = require('./back/controllers/binuto.controller')
 //Cor-----------------------------------------------------------------
 const cor = require('./back/settings/cores')
-
 // Mqtt --------------------------------------------------------------
 const mqtts = require('./back/mqtt/config/mqttconfig')
-
-// const mqtt = require('mqtt')
-// var mqtt = mqtts.mqtt
-// var options = mqtts.options
-
 const cav = require('./cav')
-
+//--------------------------------------------------------------------
 const clientbin = mqtts.connecting
 const clientcon = mqtts.connecting
 const env = mqtts.envs
@@ -25,45 +19,57 @@ const env = mqtts.envs
 // Potência = 0,143 kW ou 143 W
 // var client = mqtt.connect(, options);
 
-
-// clientbin.on('connect', function () {
-//     clientbin.subscribe(env.Topic, function () {
-//         // Quando a messagem chegar, esse função sera executada. 
-//         clientbin.on('message', function (topic, message, packet) {
-//             var message_str = JSON.parse(message)
-//             // console.log('Imprimindo do APP ->' ,message_str)
-//             // data = message_str.DATA
-//             // console.log(data)
-//             console.log(`${cor.FgGreen}BinCtrl${cor.Reset}`)
-//             binctrl.InsertsMqtt(message_str)
-//             // console.log(`${cor.FgRed}ConCtrl${cor.Reset}`)
-//             // console.log(`${cav.cav(cor.FgGreen,cor.Reset)}`) // CAV****
-
-
-//             // console.log("Received '" + message + "' on '" + topic + "'");
-//         });
-//     });
-// })
-
+// const insert = function() {
 clientcon.on('connect', function () {
-    clientcon.subscribe(env.Topic, function () { // Tentativa de incersao em dois topicos. 
-    // clientcon.subscribe(env.Topic, function () {
-        // Quando a messagem chegar, esse função sera executada. 
-        clientcon.on('message', function (topic, message, packet) {
-            var message_str = JSON.parse(message)
-            // console.log('Imprimindo do APP ->' ,message_str)
-            console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgGreen}ConCtrl${cor.Reset}`)
-            // data = message_str.DATA
-            // console.log(data)
-            // console.log(`${cor.FgGreen}BinCtrl${cor.Reset}`)
-            // binctrl.InsertsMqtt(message_str)
-
-            // console.log(`${cor.FgRed}ConCtrl${cor.Reset}`)
-            // console.log(`${cav.cav(cor.FgGreen,cor.Reset)}`) // CAV****
-            mconctrl.InsertsMqtt(message_str)
-
-            // console.log("Received '" + message + "' on '" + topic + "'");
-        });
+    clientcon.subscribe(env.Topic, function () {
+        // Quando a messagem chegar, essa função sera executada. 
+        setTimeout(function () {
+            clientcon.on('message', function (topic, message, packet) {
+                var message_str = JSON.parse(message)
+                console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgGreen}ConCtrl${cor.Reset}`)
+                //inserindo no bando de dados os dados recibidos no MQTT
+                binuto.InsertsMqtt(message_str)
+            });
+        }, 10 * 1000)
     });
 })
+// }
+
+
+// setTimeout(function () {
+//     insert()
+// }, 10 * 1000)
+
+// setTimeout(function () {
+//     clientcon.on('connect', function () {
+//         clientcon.subscribe(env.Topic, function () {
+//             // Quando a messagem chegar, esse função sera executada. 
+//             clientcon.on('message', function (topic, message, packet) {
+//                 var message_str = JSON.parse(message)
+//                 console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgGreen}ConCtrl${cor.Reset}`)
+//                 //inserindo no bando de dados os dados recibidos no MQTT
+//                 binuto.InsertsMqtt(message_str)
+
+//             });
+//         });
+//     })
+// }, 30 * 1000)
+
+
+    // clientcon.on('connect', function () {
+    //     clientcon.subscribe(env.Topic, function () { 
+    //         // Quando a messagem chegar, esse função sera executada. 
+    //         clientcon.on('message', function (topic, message, packet) {
+    //             var message_str = JSON.parse(message)
+    //             console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgRed}BinCtrl${cor.Reset}`)
+    //             //inserindo no bando de dados os dados recibidos no MQTT
+    //             binctrl.InsertsMqtt(message_str)
+    //         });
+    //     });
+    // })
+
+
+
+
+
 
--- app.js
+++ app.js
@@ -19,21 +19,68 @@ const env = mqtts.envs
 // Potência = 0,143 kW ou 143 W
 // var client = mqtt.connect(, options);
 
+// const insert = function() {
+// setTimeout(function () {
+
+// }, 10 * 1000)
 
 
-clientcon.on('connect', function () {
-    clientcon.subscribe(env.Topic, function () {
-        // Quando a messagem chegar, esse função sera executada. 
-        clientcon.on('message', function (topic, message, packet) {
-            var message_str = JSON.parse(message)
-            console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgGreen}ConCtrl${cor.Reset}`)
-            //inserindo no bando de dados os dados recibidos no MQTT
-            setInterval(function () {
-                binuto.InsertsMqtt(message_str)
-            }, 30 * 1000)
-        });
-    });
-})
+
+
+
+
+
+// var count = new Number();
+var count = 5;
+// variável para o contador
+var contador;
+// var tempo = document.getElementById("tempo");
+
+function start() {
+    if ((count - 1) >= 0) {
+        count = count - 1;
+        contador = setTimeout(start, 1000);
+        console.log(`Inserindo o proximo registro em: ${count}s`);
+    }
+    else if (count == 0) {
+        console.log('Executanod função Mqtt')
+        clientcon.on('connect', function () {
+            clientcon.subscribe(env.Topic, function () {
+                // Quando a messagem chegar, essa função sera executada. 
+                // setTimeout(function () {
+                clientcon.on('message', function (topic, message, packet) {
+                    var message_str = JSON.parse(message)
+                    console.log(`${cor.FgCyan}Imprimindo do APP ->${cor.FgYellow} Registro inserido ${cor.FgBlack}${cor.BgGreen}ConCtrl${cor.Reset}`)
+                    //inserindo no bando de dados os dados recibidos no MQTT
+                    binuto.InsertsMqtt(message_str)
+                });
+                // }, 10 * 1000)
+            });
+        })
+    }
+}
+
+function stop() {
+    // tempo.innerText = "Parado em " + count;
+    clearInterval(contador);
+}
+
+start();
+// stop();
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNCIONAIS -----------------------------------------------------------------------------
+
 
     // clientcon.on('connect', function () {
     //     clientcon.subscribe(env.Topic, function () { 
